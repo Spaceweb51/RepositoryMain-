c35452163994f1149d2f7be11e538a6450f48e11
Spaceweb51 <80417405+Spaceweb51@users.noreply.github.com>
2021-03-22 18:57:06 +0100

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,14 @@
-
+<header class="header_content">
+	<div class="logo_gbaf">
+		<a href="accueil.php"><img src="../pages/logos/gbaf.png" title="GBAF" alt="GBAF logo"/></a>
+	</div>
+		<div class="user_name">
+			<a href="profil.php" title="Voir mon profil"><p>prenom nom</p></a>
+		</div>
+			<form class="deconnection_form" action="traitement/trait_deconnexion.php" method="post"><input type="submit" value="deconnexion"/></form>				
+		</div>
+			
+		<div class="inscription_link">
+			<a href="inscription.php">S'inscrire</a><p>/</p><a href="accueil.php">Se connecter</a>
+		</div>		
+</header>
