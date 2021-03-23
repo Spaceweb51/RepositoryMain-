62962c92e20589063839f66a8b1728e6455af8a1
Spaceweb51 <80417405+Spaceweb51@users.noreply.github.com>
2021-03-23 12:31:30 +0100

    Add files via upload

---
+++ public/contact.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="icon" type="image/png" href="logos/gbaf_ico.png" />
+		<title>Contact</title>
+	</head>
+	<body>
+		<?php include("../includes/header.php"); ?>
+		<section class="content contact_content">
+			<h1>CONTACT</h1>
+		</section>
+		<?php include("../includes/footer.php"); ?>
+	</body>
+</html>
